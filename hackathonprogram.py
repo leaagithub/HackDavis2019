84ecf56b1e423d23152b3c4ea6206170e877f876
Aaron Le <aaronle123@gmail.com>
2019-02-10 05:03:59 +0000

    Program Code. Several Bugs

---
+++ hackathonprogram.py
@@ -0,0 +1,59 @@
+#Hackathon DNA Program 
+import re
+
+DNASeq = ''
+SSR = ''
+iteratorCount = 0
+indexlist = []
+insertionCount = 0
+checkCount = 0 
+zeroCount = 0 
+
+def getDNASeq():
+    f = open('DNASeqList.txt', 'r')
+    DNASeq = f.readline()
+    print "DNA Sequence List is: " ,DNASeq
+    return DNASeq
+def writeHeader():
+    f = open('output.txt', 'w')
+    header = 'SSR,Count, indexes \n'
+    f.writelines(header)
+def writeOutput(SSR,iteratorCount,indexlist):
+    line1 = ''
+    f = open('output.txt', 'a')
+    for x in indexlist:
+        str1 = ','.join(str(e) for e in x)
+        line1 = line1 + "|" +  str1
+    #print "list: " + line1
+    line = SSR + ',' + str(iteratorCount) + ',' + line1 + '\n'
+    f.write(line)
+
+#END FUNCTIONS
+
+DNASeq = getDNASeq();
+writeHeader();
+with open('SSRList.txt','r') as f:
+    for line in f:
+        line = line.rstrip('\n')
+        #print line
+        m = re.compile(line)
+        print m
+        #translates string into object
+        print DNASeq
+        for match in m.finditer(DNASeq):
+            print 'for loop check'
+            #MATCHES SSR INTO DATA SEQUENCES AND INCREMENTS IF FOUND, GOES NEXT
+            iteratorCount += 1
+            #print match.span()
+            l = list(match.span())
+            #print l
+            indexlist.append(l)
+            #print indexes
+        print iteratorCount
+        print line
+        print indexlist
+        writeOutput(line,iteratorCount,indexlist)
+        iteratorCount = 0
+        del indexlist[:]
+        checkCount += 1
+print "Done checking SSR List. Total SSR Checked: ",checkCount, "Total Found is: ",insertionCount
---
+++ test.py
@@ -0,0 +1,45 @@
+#Hackathon DNA Program 
+import re
+
+DNASeq = ''
+SSR = 'A'
+iteratorCount = 0
+indexlist = []
+
+def getDNASeq():
+    f = open('DNASeqList.txt', 'r')
+    DNASeq = f.readline()
+    print "DNA Sequence List is: " ,DNASeq
+    return DNASeq
+def writeHeader():
+    f = open('output.txt', 'w')
+    header = 'SSR,Count, indexes \n'
+    f.writelines(header)
+def writeOutput(SSR,iteratorCount,indexlist):
+    line1 = ''
+    f = open('output.txt', 'a')
+    for x in indexlist:
+        str1 = ','.join(str(e) for e in x)
+        line1 = line1 + "|" +  str1
+    print "list: " + line1
+    line = SSR + ',' + str(iteratorCount) + ',' + line1 + '\n'
+    f.write(line)
+#END FUNCTIONS
+
+DNASeq = getDNASeq();
+writeHeader();
+m = re.compile(SSR)
+print m
+#translates string into object
+iterator = m.finditer(DNASeq)
+for match in iterator:
+    iteratorCount += 1
+    #print match.span()
+    l = list(match.span())
+    #print l
+    indexlist.append(l)
+#print indexes
+print iteratorCount
+print SSR
+print indexlist
+writeOutput(SSR,iteratorCount,indexlist)
